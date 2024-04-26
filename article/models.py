c0c6891d17515ad9d65c65eff81e75c35e8def98
bashizade <bashizade.official@gmail.com>
2024-04-26 15:48:46 +0330

    fix

--- article/models.py
+++ article/models.py
@@ -1,3 +1,23 @@
+from django.utils import timezone
+
 from django.db import models
+from category.models import Category
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255, verbose_name="عنوان")
+    info = models.TextField(verbose_name="خلاصه مطلب")
+    description = models.TextField(verbose_name="متن خبر")
+    image = models.ImageField(upload_to="article", verbose_name="عکس شاخص")
+    hashtags = models.TextField(verbose_name="هشتگ ها")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "نوشته"
+        verbose_name_plural = "نوشته ها"
--- category/models.py
+++ category/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name="عنوان")
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
