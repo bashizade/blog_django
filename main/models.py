ade6e5158e99d08b6af0f250368aa15c4662fd94
bashizade <bashizade.official@gmail.com>
2024-04-29 11:21:28 +0430

    fix

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Slider(models.Model):
+    image = models.TextField(verbose_name="تصویر اسلاید")
+    status = models.BooleanField(default=True, verbose_name="وضعیت نمایش")
+
+
